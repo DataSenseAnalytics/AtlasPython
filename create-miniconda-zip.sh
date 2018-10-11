e806b03d0d4b99d11a92d04da17e6c25e25cd2f8
Jacob Roberts <jacobdr@gmail.com>
2018-10-11 16:24:22 -0700

    Update scripts for creating os x and linux conda tarballs

--- create-miniconda-zip.sh
+++ create-miniconda-zip.sh
@@ -3,30 +3,63 @@
 # This is useful when we want to add a new zip version to the DataSenseAnalytics/AtlasPython
 # repository
 
-set -euo pipefail
-
-# https://stackoverflow.com/a/246128
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 MINICONDA_VERSION="miniconda2-4.3.30"
-
-pyenv uninstall -f "${MINICONDA_VERSION}"
-pyenv install "${MINICONDA_VERSION}"
-
-pushd ~/.pyenv/versions/"${MINICONDA_VERSION}"
-
-MINICONDA_DIR="$(pwd)"
-PATH="${MINICONDA_DIR}/bin:${PATH}"
-echo "MINICONDA_DIR=${MINICONDA_DIR}"
 
 function conda_pip() {
     python -m pip "$@"
 }
 
-conda_pip install --upgrade pip
+function create_miniconda_tarball_generic() {
+    # https://stackoverflow.com/a/246128
+    ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-OS_VERSION_ARCH="$(uname)_$(uname -p)"
-OUTPUT_ZIP_NAME="${ROOT_DIR}/${MINICONDA_VERSION}_${OS_VERSION_ARCH}.tgz"
-tar -C "${MINICONDA_DIR}/.." -czf  "${OUTPUT_ZIP_NAME}" "${MINICONDA_VERSION}"
+    pyenv uninstall -f "${MINICONDA_VERSION}"
+    pyenv install "${MINICONDA_VERSION}"
 
-echo "Miniconda installation for platform ${OS_VERSION_ARCH} complete"
-echo "Artifact is available at: ${OUTPUT_ZIP_NAME}"
+    pushd ~/.pyenv/versions/"${MINICONDA_VERSION}"
+
+    MINICONDA_DIR="$(pwd)"
+    PATH="${MINICONDA_DIR}/bin:${PATH}"
+    echo "MINICONDA_DIR=${MINICONDA_DIR}"
+
+
+
+    conda_pip install --upgrade pip
+
+    OS_VERSION_ARCH="$(uname)_$(uname -p)"
+    OUTPUT_ZIP_NAME="${ROOT_DIR}/${MINICONDA_VERSION}_${OS_VERSION_ARCH}.tgz"
+    tar -C "${MINICONDA_DIR}/.." -czf  "${OUTPUT_ZIP_NAME}" "${MINICONDA_VERSION}"
+
+    echo "Miniconda installation for platform ${OS_VERSION_ARCH} complete"
+    echo "Artifact is available at: ${OUTPUT_ZIP_NAME}"
+}
+
+function create_miniconda_tarball_osx() {
+    create_miniconda_tarball_generic
+}
+
+function create_miniconda_tarball_linux() {
+    local docker_image_name="atlas-python:${MINICONDA_VERSION}"
+    docker build -t  "${docker_image_name}" .
+    # In order to use the docker cp command need to supply a container name.
+    # We create a random one
+    docker run -it "${docker_image_name}" bash -c "exit 0"
+    local random_container_name
+    random_container_name="$(echo ${RANDOM})"
+    local conda_location_inside_image="/conda"
+    docker cp "${random_container_name}:/${conda_location_inside_image}/." .
+}
+
+function main() {
+    set -euo pipefail
+    create_miniconda_tarball_linux
+    create_miniconda_tarball_osx
+}
+
+# https://stackoverflow.com/questions/2683279/how-to-detect-if-a-script-is-being-sourced
+if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+    echo "script is being sourced ..."
+else
+    main
+fi
+
